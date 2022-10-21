89b5185815ce5be18bfb4765df705a88d877e406
Valkorian <marcopolo9420@gmail.com>
2022-10-21 11:15:58 +0100

    dialog system works

--- Assets/Scripts/Game 2 scripts/Dialog.cs
+++ Assets/Scripts/Game 2 scripts/Dialog.cs
@@ -10,11 +10,14 @@ public class Dialog : MonoBehaviour
     private int index;
     public float typingSpeed;
     public GameObject continueButton;
+    public GameObject Panel;
+    //public bool activeConversation = false;
     // Start is called before the first frame update
     void Start()
     {
       dialogText.text = "";
-        StartCoroutine(Type());
+        //Panel.SetActive(true);
+        //StartCoroutine(Type());
     }
 
     // Update is called once per frame
@@ -45,8 +48,27 @@ public class Dialog : MonoBehaviour
         else{
             dialogText.text = "";
              continueButton.SetActive(false);
+            Panel.SetActive(false);
+            //activeConversation = false;
         }
     }
-       
-    
+
+   /* private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Conversation" &&!activeConversation)
+        {
+            
+            activeConversation = true;
+        }
+        
+    }
+    */
+
+    public void StartConversation()
+    {
+        Panel.SetActive(true);
+        StartCoroutine(Type());
+    }
+
+
 }
